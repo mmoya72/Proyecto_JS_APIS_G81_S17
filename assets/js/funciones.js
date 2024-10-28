24e2969efa87f1b441d63b7dae842cabee0e05e3
mmoya72 <mmoya72@gmail.com>
2024-10-28 20:26:24 -0300

    G81 JS API

---
+++ assets/js/funciones.js
@@ -0,0 +1,779 @@
+
+const monedas = { "supported_codes":[[
+  "AED",
+  "UAE Dirham"
+],
+[
+  "AFN",
+  "Afghan Afghani"
+],
+[
+  "ALL",
+  "Albanian Lek"
+],
+[
+  "AMD",
+  "Armenian Dram"
+],
+[
+  "ANG",
+  "Netherlands Antillian Guilder"
+],
+[
+  "AOA",
+  "Angolan Kwanza"
+],
+[
+  "ARS",
+  "Argentine Peso"
+],
+[
+  "AUD",
+  "Australian Dollar"
+],
+[
+  "AWG",
+  "Aruban Florin"
+],
+[
+  "AZN",
+  "Azerbaijani Manat"
+],
+[
+  "BAM",
+  "Bosnia and Herzegovina Convertible Mark"
+],
+[
+  "BBD",
+  "Barbados Dollar"
+],
+[
+  "BDT",
+  "Bangladeshi Taka"
+],
+[
+  "BGN",
+  "Bulgarian Lev"
+],
+[
+  "BHD",
+  "Bahraini Dinar"
+],
+[
+  "BIF",
+  "Burundian Franc"
+],
+[
+  "BMD",
+  "Bermudian Dollar"
+],
+[
+  "BND",
+  "Brunei Dollar"
+],
+[
+  "BOB",
+  "Bolivian Boliviano"
+],
+[
+  "BRL",
+  "Brazilian Real"
+],
+[
+  "BSD",
+  "Bahamian Dollar"
+],
+[
+  "BTN",
+  "Bhutanese Ngultrum"
+],
+[
+  "BWP",
+  "Botswana Pula"
+],
+[
+  "BYN",
+  "Belarusian Ruble"
+],
+[
+  "BZD",
+  "Belize Dollar"
+],
+[
+  "CAD",
+  "Canadian Dollar"
+],
+[
+  "CDF",
+  "Congolese Franc"
+],
+[
+  "CHF",
+  "Swiss Franc"
+],
+[
+  "CLP",
+  "Chilean Peso"
+],
+[
+  "CNY",
+  "Chinese Renminbi"
+],
+[
+  "COP",
+  "Colombian Peso"
+],
+[
+  "CRC",
+  "Costa Rican Colon"
+],
+[
+  "CUP",
+  "Cuban Peso"
+],
+[
+  "CVE",
+  "Cape Verdean Escudo"
+],
+[
+  "CZK",
+  "Czech Koruna"
+],
+[
+  "DJF",
+  "Djiboutian Franc"
+],
+[
+  "DKK",
+  "Danish Krone"
+],
+[
+  "DOP",
+  "Dominican Peso"
+],
+[
+  "DZD",
+  "Algerian Dinar"
+],
+[
+  "EGP",
+  "Egyptian Pound"
+],
+[
+  "ERN",
+  "Eritrean Nakfa"
+],
+[
+  "ETB",
+  "Ethiopian Birr"
+],
+[
+  "EUR",
+  "Euro"
+],
+[
+  "FJD",
+  "Fiji Dollar"
+],
+[
+  "FKP",
+  "Falkland Islands Pound"
+],
+[
+  "FOK",
+  "Faroese Króna"
+],
+[
+  "GBP",
+  "Pound Sterling"
+],
+[
+  "GEL",
+  "Georgian Lari"
+],
+[
+  "GGP",
+  "Guernsey Pound"
+],
+[
+  "GHS",
+  "Ghanaian Cedi"
+],
+[
+  "GIP",
+  "Gibraltar Pound"
+],
+[
+  "GMD",
+  "Gambian Dalasi"
+],
+[
+  "GNF",
+  "Guinean Franc"
+],
+[
+  "GTQ",
+  "Guatemalan Quetzal"
+],
+[
+  "GYD",
+  "Guyanese Dollar"
+],
+[
+  "HKD",
+  "Hong Kong Dollar"
+],
+[
+  "HNL",
+  "Honduran Lempira"
+],
+[
+  "HRK",
+  "Croatian Kuna"
+],
+[
+  "HTG",
+  "Haitian Gourde"
+],
+[
+  "HUF",
+  "Hungarian Forint"
+],
+[
+  "IDR",
+  "Indonesian Rupiah"
+],
+[
+  "ILS",
+  "Israeli New Shekel"
+],
+[
+  "IMP",
+  "Manx Pound"
+],
+[
+  "INR",
+  "Indian Rupee"
+],
+[
+  "IQD",
+  "Iraqi Dinar"
+],
+[
+  "IRR",
+  "Iranian Rial"
+],
+[
+  "ISK",
+  "Icelandic Króna"
+],
+[
+  "JEP",
+  "Jersey Pound"
+],
+[
+  "JMD",
+  "Jamaican Dollar"
+],
+[
+  "JOD",
+  "Jordanian Dinar"
+],
+[
+  "JPY",
+  "Japanese Yen"
+],
+[
+  "KES",
+  "Kenyan Shilling"
+],
+[
+  "KGS",
+  "Kyrgyzstani Som"
+],
+[
+  "KHR",
+  "Cambodian Riel"
+],
+[
+  "KID",
+  "Kiribati Dollar"
+],
+[
+  "KMF",
+  "Comorian Franc"
+],
+[
+  "KRW",
+  "South Korean Won"
+],
+[
+  "KWD",
+  "Kuwaiti Dinar"
+],
+[
+  "KYD",
+  "Cayman Islands Dollar"
+],
+[
+  "KZT",
+  "Kazakhstani Tenge"
+],
+[
+  "LAK",
+  "Lao Kip"
+],
+[
+  "LBP",
+  "Lebanese Pound"
+],
+[
+  "LKR",
+  "Sri Lanka Rupee"
+],
+[
+  "LRD",
+  "Liberian Dollar"
+],
+[
+  "LSL",
+  "Lesotho Loti"
+],
+[
+  "LYD",
+  "Libyan Dinar"
+],
+[
+  "MAD",
+  "Moroccan Dirham"
+],
+[
+  "MDL",
+  "Moldovan Leu"
+],
+[
+  "MGA",
+  "Malagasy Ariary"
+],
+[
+  "MKD",
+  "Macedonian Denar"
+],
+[
+  "MMK",
+  "Burmese Kyat"
+],
+[
+  "MNT",
+  "Mongolian Tögrög"
+],
+[
+  "MOP",
+  "Macanese Pataca"
+],
+[
+  "MRU",
+  "Mauritanian Ouguiya"
+],
+[
+  "MUR",
+  "Mauritian Rupee"
+],
+[
+  "MVR",
+  "Maldivian Rufiyaa"
+],
+[
+  "MWK",
+  "Malawian Kwacha"
+],
+[
+  "MXN",
+  "Mexican Peso"
+],
+[
+  "MYR",
+  "Malaysian Ringgit"
+],
+[
+  "MZN",
+  "Mozambican Metical"
+],
+[
+  "NAD",
+  "Namibian Dollar"
+],
+[
+  "NGN",
+  "Nigerian Naira"
+],
+[
+  "NIO",
+  "Nicaraguan Córdoba"
+],
+[
+  "NOK",
+  "Norwegian Krone"
+],
+[
+  "NPR",
+  "Nepalese Rupee"
+],
+[
+  "NZD",
+  "New Zealand Dollar"
+],
+[
+  "OMR",
+  "Omani Rial"
+],
+[
+  "PAB",
+  "Panamanian Balboa"
+],
+[
+  "PEN",
+  "Peruvian Sol"
+],
+[
+  "PGK",
+  "Papua New Guinean Kina"
+],
+[
+  "PHP",
+  "Philippine Peso"
+],
+[
+  "PKR",
+  "Pakistani Rupee"
+],
+[
+  "PLN",
+  "Polish Złoty"
+],
+[
+  "PYG",
+  "Paraguayan Guaraní"
+],
+[
+  "QAR",
+  "Qatari Riyal"
+],
+[
+  "RON",
+  "Romanian Leu"
+],
+[
+  "RSD",
+  "Serbian Dinar"
+],
+[
+  "RUB",
+  "Russian Ruble"
+],
+[
+  "RWF",
+  "Rwandan Franc"
+],
+[
+  "SAR",
+  "Saudi Riyal"
+],
+[
+  "SBD",
+  "Solomon Islands Dollar"
+],
+[
+  "SCR",
+  "Seychellois Rupee"
+],
+[
+  "SDG",
+  "Sudanese Pound"
+],
+[
+  "SEK",
+  "Swedish Krona"
+],
+[
+  "SGD",
+  "Singapore Dollar"
+],
+[
+  "SHP",
+  "Saint Helena Pound"
+],
+[
+  "SLE",
+  "Sierra Leonean Leone"
+],
+[
+  "SLL",
+  "Sierra Leonean Leone"
+],
+[
+  "SOS",
+  "Somali Shilling"
+],
+[
+  "SRD",
+  "Surinamese Dollar"
+],
+[
+  "SSP",
+  "South Sudanese Pound"
+],
+[
+  "STN",
+  "São Tomé and Príncipe Dobra"
+],
+[
+  "SYP",
+  "Syrian Pound"
+],
+[
+  "SZL",
+  "Eswatini Lilangeni"
+],
+[
+  "THB",
+  "Thai Baht"
+],
+[
+  "TJS",
+  "Tajikistani Somoni"
+],
+[
+  "TMT",
+  "Turkmenistan Manat"
+],
+[
+  "TND",
+  "Tunisian Dinar"
+],
+[
+  "TOP",
+  "Tongan Paʻanga"
+],
+[
+  "TRY",
+  "Turkish Lira"
+],
+[
+  "TTD",
+  "Trinidad and Tobago Dollar"
+],
+[
+  "TVD",
+  "Tuvaluan Dollar"
+],
+[
+  "TWD",
+  "New Taiwan Dollar"
+],
+[
+  "TZS",
+  "Tanzanian Shilling"
+],
+[
+  "UAH",
+  "Ukrainian Hryvnia"
+],
+[
+  "UGX",
+  "Ugandan Shilling"
+],
+[
+  "USD",
+  "United States Dollar"
+],
+[
+  "UYU",
+  "Uruguayan Peso"
+],
+[
+  "UZS",
+  "Uzbekistani So'm"
+],
+[
+  "VES",
+  "Venezuelan Bolívar Soberano"
+],
+[
+  "VND",
+  "Vietnamese Đồng"
+],
+[
+  "VUV",
+  "Vanuatu Vatu"
+],
+[
+  "WST",
+  "Samoan Tālā"
+],
+[
+  "XAF",
+  "Central African CFA Franc"
+],
+[
+  "XCD",
+  "East Caribbean Dollar"
+],
+[
+  "XDR",
+  "Special Drawing Rights"
+],
+[
+  "XOF",
+  "West African CFA franc"
+],
+[
+  "XPF",
+  "CFP Franc"
+],
+[
+  "YER",
+  "Yemeni Rial"
+],
+[
+  "ZAR",
+  "South African Rand"
+],
+[
+  "ZMW",
+  "Zambian Kwacha"
+],
+[
+  "ZWL",
+  "Zimbabwean Dollar"
+]] }
+
+function generarFechasDecrecientes(dias) {
+  let fechas = [];
+  const hoy = new Date();
+
+  for (let i = 0; i < dias; i++) {
+    const fecha = new Date(hoy);
+    fecha.setDate(hoy.getDate() - i); 
+
+    const year = fecha.getFullYear();
+    const month = String(fecha.getMonth() + 1).padStart(2, '0');
+    const day = String(fecha.getDate()).padStart(2, '0');
+    const fechaFormateada = `${year}-${month}-${day}`;   
+
+
+    fechas.push(fechaFormateada);
+  }
+
+  return fechas;
+}
+
+const fechasDecrecientes = generarFechasDecrecientes(10);
+
+function formatearMoneda(input) {
+    // Eliminar caracteres no numéricos
+    var valor = input.value.replace(/[^0-9]/g, '');
+  
+    // Formatear el valor con separadores de miles
+    valor = new Intl.NumberFormat('es-CL').format(valor); // Formato para Chile
+  
+    // Actualizar el valor del input
+    input.value = valor;
+}
+
+const apiURL = "https://v6.exchangerate-api.com/v6/3738b2b5e9f5d83df2727946/latest/CLP";
+
+async function getCoin() {
+  const res = await fetch(apiURL);
+    divisas = await res.json();
+    return divisas;
+}
+
+async function renderMonedas() {
+  var monto_convertir =  document.querySelector(".coin");
+  var monto_num = monto_convertir.value.replace(".","");
+  
+  if (monto_num == "0"){
+    alert("Conversor de Monedas te informa: debes ingresar el monto en pesos a Convertir!!")
+    return false;
+  }
+  const coins = await getCoin();
+  const claves = Object.keys(coins.conversion_rates);
+  let comboBox = document.querySelector(".divisas_combo_box");
+  let resultado_texto = document.querySelector(".resultado_accion");
+  let indiceSeleccionado = comboBox.selectedIndex;
+  let monedaSeleccionada = comboBox.options[indiceSeleccionado].text;
+  let codigoSeleccionado = monedaSeleccionada.substring(0, 3);
+  
+  claves.forEach((coin) => {
+    let valor = coins.conversion_rates[coin];
+    if (coin === codigoSeleccionado) {
+          let conversion = Number(valor) * Number(monto_num);
+          conversion = new Intl.NumberFormat('es-CL').format(conversion); // Formato para Chile
+          resultado_texto.innerHTML = 'Resultado $' + conversion.toString();
+    }
+ });
+ renderGrafica();
+}
+
+function llenarComboBox(datos) {
+    let comboBox = document.querySelector(".divisas_combo_box");
+    let valor = 0;
+    
+    for (let i = 0; i < datos.supported_codes.length; i++) {
+      valor += 1;
+      const opcion = document.createElement('option');
+      opcion.value = valor; 
+      opcion.text = datos.supported_codes[i][0] + " - " + datos.supported_codes[i][1];  
+      comboBox.appendChild(opcion);
+
+    }
+};
+
+llenarComboBox(monedas);
+
+// Obtener el botón por su ID
+const boton = document.querySelector('.boton_accion');
+// Agregar un event listener al botón
+boton.addEventListener('click', renderMonedas);
+
+
+async function getMonedas() {
+  const endpoint = "https://api.gael.cloud/general/public/monedas";
+  const res = await fetch(endpoint);
+  const monedas = await res.json();
+  return monedas;
+}
+
+  function prepararConfiguracionParaLaGrafica(monedas) {
+    // Creamos las variables necesarias para el objeto de configuración
+    const tipoDeGrafica = "line";
+    const nombresDeLasMonedas = monedas.map((moneda) => moneda.Codigo);
+    const titulo = "Monedas";
+    const colorDeLinea = "red";
+    const valores = monedas.map((moneda) => {
+      const valor = moneda.Valor.replace(",", ".");
+      return Number(valor);
+    });
+  // Creamos el objeto de configuración usando las variables anteriores
+    const config = {
+    type: tipoDeGrafica,
+    data: {
+    labels: nombresDeLasMonedas,
+    datasets: [  {
+      label: titulo,
+      backgroundColor: colorDeLinea,
+      data: valores
+      }
+              ]
+    } };
+    return config;
+}
+
+async function renderGrafica() {
+      const monedas = await getMonedas();
+      const config = prepararConfiguracionParaLaGrafica(monedas);
+      const chartDOM = document.getElementById("myChart");
+      new Chart(chartDOM, config);
+}
+      
